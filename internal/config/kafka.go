2e21e03eafa322608d7980cd17aad007bb1159ba
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2024-02-06 11:44:39 +0700

    add config

--- internal/config/kafka.go
+++ internal/config/kafka.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"github.com/segmentio/kafka-go"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func NewKafkaReader(config *viper.Viper, log *logrus.Logger) *kafka.Reader {
+	kafkaReaderConfig := kafka.ReaderConfig{
+		Brokers: config.GetStringSlice("kafka.bootstrap.servers"),
+		GroupID: config.GetString("kafka.group.id"),
+	}
+
+	reader := kafka.NewReader(kafkaReaderConfig)
+
+	return reader
+}
+
+func NewKafkaWriter(config *viper.Viper, log *logrus.Logger) *kafka.Writer {
+	kafkaWriterConfig := &kafka.WriterConfig{
+		Brokers:  config.GetStringSlice("kafka.bootstrap.servers"),
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	writer := kafka.NewWriter(*kafkaWriterConfig)
+
+	return writer
+}
